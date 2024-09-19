5c90a567707ba3ea8653c535ca13213812130140
binhlo <binhlothanh0111@gmail.com>
2024-09-19 21:52:38 +0700

    first

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    // Phương thức main để chạy chương trình
+    public static void main(String[] args) {
+        Sum sumCalculator = new Sum(); // Tạo một đối tượng Sum
+        sumCalculator.sumNumber(); // Tính tổng các số chẵn
+        sumCalculator.printSumNumber(); // In ra tổng
+    }
+}
